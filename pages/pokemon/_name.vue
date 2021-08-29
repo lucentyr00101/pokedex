193fa70939c90eb6bacd175bd80d4ba624f7dfab
Ranz <sarcosredilbertranz@gmail.com>
2021-08-29 13:41:10 +0800

    added overlay while loading

--- pages/pokemon/_name.vue
+++ pages/pokemon/_name.vue
@@ -1,42 +1,44 @@
 <template>
-  <b-container fluid>
-    <b-row>
-      <b-col lg="3">
-        <b-container fluid>
-          <b-row>
-            <b-col>
-              <Pokemon :details="pokemon" />
-            </b-col>
-          </b-row>
-          <b-row class="form-group">
-            <b-col lg="12">
-              <Types :types="pokemon.strsweaks" />
-            </b-col>
-          </b-row>
-        </b-container>
-      </b-col>
-      <b-col lg="9">
-        <b-container fluid>
-          <b-row class="form-group">
-            <b-col>
-              <Characteristics :characteristics="pokemon.characteristics" />
-            </b-col>
-          </b-row>
-          <b-row class="form-group">
-            <b-col lg="3">
-              <Stats :stats="pokemon.stats" />
-            </b-col>
-            <b-col lg="3">
-              <Moves :moves="pokemon.moves" />
-            </b-col>
-            <b-col lg="6">
-              <Evolution :evolution="pokemon.evolution" />
-            </b-col>
-          </b-row>
-        </b-container>
-      </b-col>
-    </b-row>
-  </b-container>
+  <b-overlay :show="busy">
+    <b-container fluid>
+      <b-row>
+        <b-col lg="3">
+          <b-container fluid>
+            <b-row>
+              <b-col>
+                <Pokemon :details="pokemon" />
+              </b-col>
+            </b-row>
+            <b-row class="form-group">
+              <b-col lg="12">
+                <Types :types="pokemon.strsweaks" />
+              </b-col>
+            </b-row>
+          </b-container>
+        </b-col>
+        <b-col lg="9">
+          <b-container fluid>
+            <b-row class="form-group">
+              <b-col>
+                <Characteristics :characteristics="pokemon.characteristics" />
+              </b-col>
+            </b-row>
+            <b-row class="form-group">
+              <b-col lg="3">
+                <Stats :stats="pokemon.stats" />
+              </b-col>
+              <b-col lg="3">
+                <Moves :moves="pokemon.moves" />
+              </b-col>
+              <b-col lg="6">
+                <Evolution :evolution="pokemon.evolution" />
+              </b-col>
+            </b-row>
+          </b-container>
+        </b-col>
+      </b-row>
+    </b-container>
+  </b-overlay>
 </template>
 
 <script>
